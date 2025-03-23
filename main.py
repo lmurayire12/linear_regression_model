5b783c6a61c70393ab9d50e557b2c109d9543ad2
lmurayire12 <l.murayire@alustudent.com>
2025-03-23 14:19:59 -0400

    main

--- main.py
+++ main.py
@@ -22,16 +22,16 @@ model = joblib.load('boston_model.pkl')
 
 # Defining input data using Pydantic BaseModel
 class HouseFeatures(BaseModel):
-    crim: confloat(ge=0.0, le=100.0)  # Crime rate
-    zn: confloat(ge=0.0, le=100.0)    # Residential land zoning
-    indus: confloat(ge=0.0, le=30.0)  # Industrial proportion
-    rm: confloat(ge=3.0, le=10.0)     # Avg number of rooms
-    age: confloat(ge=0.0, le=100.0)   # Age of buildings
-    dis: confloat(ge=1.0, le=12.0)    # Distance to employment centers
-    rad: conint(ge=1, le=24)          # Accessibility to highways
-    tax: conint(ge=100, le=800)       # Property tax rate
-    ptratio: confloat(ge=10.0, le=25.0) # Pupil-teacher ratio
-    lstat: confloat(ge=1.0, le=40.0)  # Lower-status population percentage
+    crim: confloat(ge=0.0, le=100.0)  # type: ignore # Crime rate
+    zn: confloat(ge=0.0, le=100.0)    # type: ignore # Residential land zoning
+    indus: confloat(ge=0.0, le=30.0)  # type: ignore # type: ignore # Industrial proportion
+    rm: confloat(ge=3.0, le=10.0)     # type: ignore # Avg number of rooms
+    age: confloat(ge=0.0, le=100.0)   # type: ignore # Age of buildings
+    dis: confloat(ge=1.0, le=12.0)    # type: ignore # Distance to employment centers
+    rad: conint(ge=1, le=24)          # type: ignore # Accessibility to highways
+    tax: conint(ge=100, le=800)       # type: ignore # Property tax rate
+    ptratio: confloat(ge=10.0, le=25.0) # type: ignore # Pupil-teacher ratio
+    lstat: confloat(ge=1.0, le=40.0)  # type: ignore # Lower-status population percentage
 
 @app.post('/predict')
 def predict(features: HouseFeatures):
